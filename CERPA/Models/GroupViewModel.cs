b140c1f3e9491e313741a36f2dcba6e7bf0856b0
Scott Tracy <scott.tracy.st@gmail.com>
2018-07-25 07:10:13 -0500

    New Repository for remade project with no errors

---
+++ CERPA/Models/GroupViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CERPA.Models
+{
+    public class GroupViewModel
+    {
+        
+        
+            public GroupViewModel()
+            {
+                this.UsersList = new List<SelectListItem>();
+                this.RolesList = new List<SelectListItem>();
+            }
+            [Required(AllowEmptyStrings = false)]
+            public string Id { get; set; }
+            [Required(AllowEmptyStrings = false)]
+            public string Name { get; set; }
+            public string Description { get; set; }
+            public ICollection<SelectListItem> UsersList { get; set; }
+            public ICollection<SelectListItem> RolesList { get; set; }
+        
+    }
+}
---
+++ CERPA/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Owin;
+
+namespace CERPA
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
